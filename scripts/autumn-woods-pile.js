b93b85f785bbb3c31387f8797dd4d3349f0c1040
AmberAutumnsky <SierraR@pm.me>
2025-10-03 03:08:21 +1300

    please work

--- scripts/autumn-woods-pile.js
+++ scripts/autumn-woods-pile.js
@@ -64,7 +64,7 @@ Hooks.on("renderItemSheet5e", (app, html, data) => {
   // Attempt to find the properties container
   //let propSection = html.find("input[name='system.properties.fin']").closest(".form-group").parent();
   let contentSection = html.getElementsByClassName("window-content")[0];
-  let detailsSection = contentSection.getElementsByTagName("section")[2]; // YEAH I KNOWWWW KILL ME
+  let detailsSection = contentSection.getElementsByTagName("section")[3]; // YEAH I KNOWWWW KILL ME
   if (detailsSection) {
     console.log("Found details section, I hope...");
     console.log(detailsSection);
